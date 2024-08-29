5a6817b48ee4af506ed87ca4138e80e1a0153287
Borges10002 <borges10002@gmail.com>
2024-08-28 21:52:13 -0400

    update

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import ListProducService from "../services/ListProducService";
+import ShowProductService from "../services/ShowProductService";
 
 export default class ProductsController {
-  public async index(request: Request, response: Response) {}
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listProducts = new ListProducService();
+
+    const products = listProducts.execute();
+
+    return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showProduct = new ShowProductService();
+
+    const product = showProduct.execute({ id });
+
+    return response.json(product);
+  }
 }

f8100a27a6d5a36979ca45fea3fcde69377587a8
Borges10002 <borges10002@gmail.com>
2024-08-31 16:13:35 -0400

    update

--- src/shared/typeorm/migrations/1725135052660-CreateProducts.ts
+++ src/shared/typeorm/migrations/1725135052660-CreateProducts.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateProducts1722423521472 implements MigrationInterface {
+export class CreateProducts1725135052660 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({

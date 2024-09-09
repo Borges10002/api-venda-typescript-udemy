d934ecce02689c890a36ec98687bc35465a2befb
unknown <borges10002@gmail.com>
2024-09-08 22:13:07 -0400

    update

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,12 @@
-import "reflect-metadata";
-
 import AppError from "@shared/errors/AppError";
 import "@shared/typeorm";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import "reflect-metadata";
 import routes from "./routes";
+
+import { errors } from "celebrate";
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {

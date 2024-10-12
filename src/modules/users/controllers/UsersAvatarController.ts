962b5dc1c4934217d4f3dd581522565820b2182e
unknown <borges10002@gmail.com>
2024-10-12 13:40:58 -0400

    update

---
+++ src/modules/users/controllers/UsersAvatarController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
+
+export default class UsersAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateAvatar = new UpdateUserAvatarService();
+
+    const user = await updateAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file?.filename || "",
+    });
+
+    return response.json(user);
+  }
+}
